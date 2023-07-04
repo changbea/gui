83acbcc2c79183e4752a73f05db65102ede43d88
changbea <68812384+changbea@users.noreply.github.com>
2023-07-05 01:27:17 +0900

    Add files via upload

---
+++ Heading.jsx
@@ -0,0 +1,17 @@
+import Navigate from '../Navigate'
+// import './Heading.css'
+
+export default function Heading() {
+    return (
+      <div>
+        <Navigate />
+        <div className="h1 text-center centered">
+          pursuiting equality through programming.
+        </div>
+        <div className="h5 text-center centered">
+            a ship in harbor is safe, but that is not what ships are built for.
+        </div>
+        <div>menus</div>
+      </div>
+    );
+}
